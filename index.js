f4f8c0c07ef7234827da452c27ae093cf3e2b982
Leirach <juandleon27@gmail.com>
2019-11-22 02:38:44 -0600

    added heroku script

--- index.js
+++ index.js
@@ -42,11 +42,11 @@ require('./routes/commentRoute')(app);
 require('./routes/searchRoute')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('noteit/build'));
   
     const path = require('path');
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'noteit', 'build', 'index.html'))
     })
   
   }
